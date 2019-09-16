a2bf0807827f3d123ea96bfd56dc3a6cadf313e0
Josh Wu <josh.wu@tradevan.com.tw>
2019-09-16 13:41:38 +0800

    Updates

---
+++ precache-manifest.d5a2eb40fa7d2f742f68b0e84f601e0b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4846832ab95d0bce9e0d3ea9bef01d1",
+    "url": "/Week9_note_tool/index.html"
+  },
+  {
+    "revision": "f8add3d784aeeb6dfc1b",
+    "url": "/Week9_note_tool/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "b2b2bc8c4b2f7e35d430",
+    "url": "/Week9_note_tool/static/js/2.136c2cc7.chunk.js"
+  },
+  {
+    "revision": "f8add3d784aeeb6dfc1b",
+    "url": "/Week9_note_tool/static/js/main.bbe85346.chunk.js"
+  },
+  {
+    "revision": "01516b5cf79bb5ef262f",
+    "url": "/Week9_note_tool/static/js/runtime~main.feba0a84.js"
+  },
+  {
+    "revision": "e77dcb4255c4a78535cd67dd0ef32db9",
+    "url": "/Week9_note_tool/static/media/delete.e77dcb42.svg"
+  },
+  {
+    "revision": "8cfa9c9c16c873c44cfc64d9a3c661d9",
+    "url": "/Week9_note_tool/static/media/grid_selected.8cfa9c9c.svg"
+  },
+  {
+    "revision": "ac8f0429179127397c91792a391d73b0",
+    "url": "/Week9_note_tool/static/media/hashtag.ac8f0429.svg"
+  },
+  {
+    "revision": "07c4446d92fcbcc72d89c0cab66ea08d",
+    "url": "/Week9_note_tool/static/media/list_unselected.07c4446d.svg"
+  },
+  {
+    "revision": "b22a352d4e55b12ad47ae78a9c6f9c75",
+    "url": "/Week9_note_tool/static/media/markstar.b22a352d.svg"
+  },
+  {
+    "revision": "45ca0db788318de24c2869cf8eb4a6f0",
+    "url": "/Week9_note_tool/static/media/note.45ca0db7.svg"
+  },
+  {
+    "revision": "05df976f16dd93eec137baef89a241e2",
+    "url": "/Week9_note_tool/static/media/search.05df976f.svg"
+  },
+  {
+    "revision": "870d6eca1b82b8705a078fe8d8a2c350",
+    "url": "/Week9_note_tool/static/media/star.870d6eca.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Week9_note_tool/precache-manifest.d5a2eb40fa7d2f742f68b0e84f601e0b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Week9_note_tool/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
